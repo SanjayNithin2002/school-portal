896c475f054e86f499e73e171dfcb262391348cb
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-05-21 02:32:32 +0530

    node_modules and server deleted

--- server/api/middleware/clearDirectory.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-module.exports = (folder) => {
-    fs.readdir(folder, (err, files) => {
-        if (err) throw err;
-        for (const file of files) {
-            fs.unlinkSync(folder+file);
-        }
-      });
-}
--- server/api/middleware/dateDiffInDays.js
+++
@@ -1,8 +0,0 @@
-module.exports = (startDate, endDate) => {
-    var a = new Date(startDate);
-    var b = new Date(endDate);
-    var _MS_PER_DAY = 1000 * 60 * 60 * 24;
-    var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-    var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-}
