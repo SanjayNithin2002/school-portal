135ff6cac2d871ffe2e862107f5b83e2c3485594
Chetan <vipershiraguppi@gmail.com>
2023-07-25 16:13:04 +0530

    marks analysis updated

--- school-portal/src/Pages/Exam/AddExam.jsx
+++ school-portal/src/Pages/Exam/AddExam.jsx
@@ -50,6 +50,7 @@ const AddExam = () => {
                 date:'',
                 time:'',
             };
+            console.log(item)
             updatedList.push(subjects);
             return true;
         })
--- school-portal/src/Pages/Marks/Teacher.jsx
+++ school-portal/src/Pages/Marks/Teacher.jsx
@@ -7,14 +7,14 @@ import { getMarks } from '../../actions/marks';
 import matchers from '@testing-library/jest-dom/matchers';
 const data = [
   {
-    rollno: 1,
+    rollno: 5,
     fname: "Arvind",
     lname: "M M",
     marks: 89,
     grade: "B"
   },
   {
-    rollno: 2,
+    rollno: 5,
     fname: "Dharan",
     lname: "Kumar",
     marks: 66,
@@ -53,7 +53,6 @@ const data = [
 const MarksTeacher = () => {
 
   const dispatch = useDispatch();
-
   const [standard, setStandard] = useState("");
   const [section, setSection] = useState("");
   const [subject, setSubject] = useState("");
@@ -62,26 +61,27 @@ const MarksTeacher = () => {
     dispatch(getMarks({ type: localStorage.getItem('type'), id: localStorage.getItem('id') }));
   }, [dispatch])
 
+  const standardList = [{ label: "I", value: 1 }, { label: "II", value: 2 }, { label: "III", value: 3 }, { label: "IV", value: 4 }, { label: "V", value: 5 }, { label: "VI", value: 6 }, { label: "VII", value: 7 }, { label: "VIII", value: 8 }, { label: "IX", value: 9 }, { label: "X", value: 10 }, { label: "XI", value: 11 }, { label: "XII", value: 12 }]
   const m = useSelector((state) => state.marksReducer)
-console.log(m)
+  console.log(m)
 
-// m.docs.map((i)=>{
-//   console.log(i.exam.examName)
-// })
+  // let sum = 0;
+  // for (let i = 0; i < data.length; i++) {
+  //   sum += data[i].marks;
+  // }
+  // const avg = (sum / data.length).toFixed(2);
+  // let lmark = data[0].marks;
+  // let hmark = data[0].marks;
+  // for (let i = 0; i < data.length; i++) {
+  //   if (lmark > data[i].marks)
+  //     lmark = data[i].marks;
+  //   if (hmark < data[i].marks)
+  //     hmark = data[i].marks;
+  // }
 
-  let sum = 0;
-  for (let i = 0; i < data.length; i++) {
-    sum += data[i].marks;
-  }
-  const avg = (sum / data.length).toFixed(2);
-  let lmark = data[0].marks;
-  let hmark = data[0].marks;
-  for (let i = 0; i < data.length; i++) {
-    if (lmark > data[i].marks)
-      lmark = data[i].marks;
-    if (hmark < data[i].marks)
-      hmark = data[i].marks;
-  }
+  if (!m)
+    return <div>Loading...</div>
+
   return (
     <>
       <div className="Main">
@@ -92,6 +92,7 @@ console.log(m)
             <hr style={{ border: "1px solid gray" }} />
             <div>
               <div className="row classmessage-container-1">
+
                 <div className="col-lg-3">
                   <h4>Select Class : </h4>
                 </div>
@@ -104,8 +105,10 @@ console.log(m)
                     <option value="" disabled>
                       Select Class
                     </option>
-                    <option value="VII">VII</option>
-                    <option value="VIII">VIII</option>
+                    {Array.from(new Set(m.docs.map((i) => i.exam.class.standard))).map((i) => {
+                      const item = standardList.find(item => item.value === 5);
+                      return <option value={i}>{item.label}</option>
+                    })}
                   </select>
                 </div>
 
@@ -121,10 +124,9 @@ console.log(m)
                     <option value="" disabled>
                       Select Section
                     </option>
-                    <option value="A">A</option>
-                    <option value="B">B</option>
-                    <option value="C">C</option>
-                    <option value="D">D</option>
+                    {Array.from(new Set(m.docs.map((i) => i.exam.class.section))).map((i) => {
+                      return <option value={i}>{i}</option>
+                    })}
                   </select>
                 </div>
 
@@ -140,10 +142,9 @@ console.log(m)
                     <option value="" disabled>
                       Select Subject
                     </option>
-                    <option value="A">Physics</option>
-                    <option value="B">Geography</option>
-                    <option value="C">Tamil</option>
-                    <option value="D">Biology</option>
+                    {Array.from(new Set(m.docs.map((i) => i.exam.class.subject))).map((i) => {
+                      return <option value={i}>{i}</option>
+                    })}
                   </select>
                 </div>
                 <div className="col-lg-3">
@@ -158,11 +159,9 @@ console.log(m)
                     <option value="" disabled>
                       Select Exam
                     </option>
-                    <option value="A">Monthly Exam</option>
-                    <option value="B">Revision Exam</option>
-                    <option value="C">Quarterly Exam</option>
-                    <option value="D">Half Yearly Exam</option>
-                    <option value="D">Annual Exam</option>
+                    {Array.from(new Set(m.docs.map((i) => i.exam.examName))).map((i) => {
+                      return <option value={i}>{i}</option>
+                    })}
                   </select>
                 </div>
 
@@ -195,27 +194,35 @@ console.log(m)
                   </tr>
                 </thead>
                 <tbody>
-                  {standard && section && subject && exam?
-                    <>
-                      {
-                        m.docs.map((item, index) => (
-                          <tr>
-                            <td>{index + 1}</td>
-                            <td>{item.student.firstName}</td>
-                            <td>{item.scoredMarks}</td>
-                            <td>{item.exam.maxMarks}</td>
-                            <td>{item.weightageScoredMarks}</td>
-                            <td>{item.exam.weightageMarks}</td>
-                            <td>{item.remarks}</td>
-                          </tr>
-                        ))
-                      }
+                  {
+                    standard && section && subject && exam ?
+                      <>
+                        {m.docs
+                          .filter((item) => {
+                            return (
+                              item.exam.class.standard === parseInt(standard) &&
+                              item.exam.class.section === section &&
+                              item.exam.class.subject === subject &&
+                              item.exam.examName === exam
+                            );
+                          })
+                          .map((item, index) => (
+                            <tr key={index}>
+                              <td>{index + 1}</td>
+                              <td>{item.student.firstName}</td>
+                              <td>{item.scoredMarks}</td>
+                              <td>{item.exam.maxMarks}</td>
+                              <td>{item.weightageScoredMarks}</td>
+                              <td>{item.exam.weightageMarks}</td>
+                              <td>{item.remarks}</td>
+                            </tr>
+                          ))}
 
-                    </>
-                    :
-                    <tr>
-                      <td style={{ textAlign: "center" }} colSpan={5}>No Data</td>
-                    </tr>
+                      </>
+                      :
+                      <tr>
+                        <td style={{ textAlign: "center" }} colSpan={5}>No Data</td>
+                      </tr>
                   }
                 </tbody>
               </Table>
